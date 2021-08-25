0eb783858dbbcb696a63a7bcd3be4298d8e2bb42
Abhinav2008-dotcom <75369158+Abhinav2008-dotcom@users.noreply.github.com>
2021-08-25 19:05:39 +0530

    Add files via upload

---
+++ level2.js
@@ -0,0 +1,124 @@
+function level2(){
+    console.log("level 2");
+
+   // score=0;
+
+    bg.addAnimation("level 2",bg2); 
+    bg.changeAnimation("level 2",bg2); 
+    bg.scale=1.7;
+
+
+    if(bg.y>355){
+        bg.y=250
+    }
+  
+   spawnrunFast();
+   spawnlion();
+   spawneagle();
+   spawnpanther();
+   spawnheal();
+
+
+   if(score<0){
+    gameState=4;
+}
+
+/*if(frameCount%300===0){
+    score=score+10;
+    console.log("points added");
+}*/
+
+if(lionGroup.isTouching(dyso)){
+    lionGroup.destroyEach();
+    score=score-10;
+      }
+
+      if(healGroup.isTouching(dyso)){
+        healGroup.destroyEach();
+        score=score+10;
+          }
+   
+          if(fastRunGroup.isTouching(dyso)){
+            fastRunGroup.destroyEach();
+            
+            if(keyDown("LEFT_ARROW")){
+                dyso.x-=10;
+               }
+             if (keyDown("RIGHT_ARROW")){
+                dyso.x+=10;
+               }
+             if (keyDown("UP_ARROW")){
+                dyso.y-=10;
+               }
+             if (keyDown("DOWN_ARROW")){
+                dyso.y+=10;
+               } 
+              }
+   //when dyso is touching animals----
+      
+    gorillaGroup.destroyEach();
+    rhinocerosGroup.destroyEach();
+    crocodileGroup.destroyEach();
+
+    if(score>=20){
+        score=0;
+        gameState=3;
+    }
+}
+
+function spawneagle(){
+    if(frameCount%500===0){
+    eagle=createSprite(Math.round(random(10,700)),0,20,20);
+    eagle.velocityY=1;
+    eagle.addImage("Eagle",eagleimg);
+    eagle.scale=0.4;
+    eagle.lifetime=600;
+    eagleGroup.add(eagle);
+    }
+}
+
+function spawnlion(){
+    if(frameCount%750===0){
+    lion=createSprite(Math.round(random(10,700)),0,20,20);
+    lion.velocityY=1;
+    lion.addImage("Lion",lionimg);
+    lion.scale=1;
+    lion.lifetime=600;
+    lionGroup.add(lion);
+
+    }
+}
+
+function spawnpanther(){
+    if(frameCount%1350===0){
+    panther=createSprite(Math.round(random(10,700)),0,20,20);
+    panther.velocityY=1;
+    panther.addImage("Panther",pantherimg);
+    panther.scale=0.7;
+    panther.lifetime=600;
+    pantherGroup.add(panther)
+    }
+}
+
+function spawnrunFast(){
+    if(frameCount%1000===0) {
+        //change framecount to 1500!!!!!!!!!!!!!!!!!
+    fastrun=createSprite(Math.round(random(10,700)),0,20,20);
+    fastrun.velocityY=1;
+    fastrun.addImage("Run",fastrunimg);
+    fastrun.lifetime=1200; 
+    fastrun.scale=0.3
+    fastRunGroup.add(fastrun)
+    }
+}
+
+function spawnheal(){
+    if(frameCount%100===0){
+    heal=createSprite(Math.round(random(10,700)),0,20,20);
+    heal.velocityY=1
+    heal.addImage("Heal",healimg);
+    heal.lifetime=2700;
+    heal.scale=0.1;
+    healGroup.add(heal);
+    }
+}
